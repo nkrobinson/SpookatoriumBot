c334c8105adb1074192c43c5b600503b4c261b6c
Nicholas Robinson <kylenr23@gmail.com>
2021-10-17 17:37:21 +0100

    Added optional channel_id parameter for joinchannel Command

--- commands/joinchannel.js
+++ commands/joinchannel.js
@@ -4,18 +4,21 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('joinchannel')
 		.setDescription('Joins the voice channel user is currently connected to')
+		.addChannelOption(option => option.setName('channel').setDescription('Enter a Voice Channel to join'))
 		.setDefaultPermission(false),
     async execute({interaction, voice}={}) {
 
-        if (interaction.member.voice.channel == null)
-            return interaction.reply({ content: `User ${interaction.user.name} is not in a Voice Channel`, ephemeral: true });
+        const channelId = interaction.options.getChannel('channel') ?? interaction.member.voice.channel?.id;
+        
+        if (channelId == null)
+            return interaction.reply({ content: `User is not in a Voice Channel and Channel ID not filled`, ephemeral: true });
 
         voice.connect(
-            interaction.member.voice.channel.id,
+            channelId.id,
             interaction.guild.id,
             interaction.guild.voiceAdapterCreator
         );
         
-        return interaction.reply({ content: `Connected to Channel ${interaction.member.voice.channel.name}`, ephemeral: true });
+        return interaction.reply({ content: `Connected to Channel ${channelId.name}`, ephemeral: true });
 	},
 };
