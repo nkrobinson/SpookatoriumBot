d9346fc22b8855785dcd769884406d090a1ef6d0
Nicholas Robinson <kylenr23@gmail.com>
2022-10-10 21:35:10 +0100

    Added Twitch Bot to index - Added functionality to connect to Twitch - TODO: Add Specific run commands to limit uses to Discord or Twitch

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
+const tmi = require('tmi.js');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token, guildId, roleId } = require('./config/config.json');
+const { token, twitchUsername, twitchOAuth, twitchChannel } = require('./config/config.json');
 const { Voice } = require('./modules/voice.js');
 const { Voting } = require('./modules/voting.js');
 const { Bridge } = require('./modules/bridge.js');
 const { Award } = require('./modules/award.js');
 
+////// DISCORD CLIENT //////
+// Initialise Discord Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] });
 
+// Set up Discord Commands
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
@@ -18,13 +21,12 @@ for (const file of commandFiles) {
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-
     //TODO: Add permissions for Admin only commands
     // Currently Admin permissions have to be given manually
-
 	console.log('Ready!');
 });
 
+// Handle Discord Interactions
 client.on('interactionCreate', async interaction => {
 	if (interaction.isCommand()) {
 		const command = client.commands.get(interaction.commandName);
@@ -76,8 +78,45 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+////// TWITCH CLIENT //////
+
+// Define configuration options
+const opts = {
+	connection: {
+		reconnect: true
+	},
+	identity: {
+		username: twitchUsername,
+		password: twitchOAuth
+	},
+	channels: [
+		twitchChannel
+	]
+};
+
+// Create a Twitch client with our options
+const t_client = new tmi.client(opts);
+
+// Twitch client events
+t_client.on('message',   onMessageHandler);
+t_client.on('connected', onConnectedHandler);
+
+// Twitch command handlers
+function onMessageHandler (target, context, msg, self) {
+    if (self) return;
+    
+	// This logs out all the messages sent on the channel on the terminal
+    console.log(msg);
+	t_client.say(twitchChannel, `YOUR MESSAGE WAS ${msg}`);
+}
+
+function onConnectedHandler(addr, port) {
+	console.log('Connected');
+}
+
 const award = new Award();
 client.login(token);
+t_client.connect();
 const voice = new Voice(client);
 const voting = new Voting();
 new Bridge(client, voice, voting);
