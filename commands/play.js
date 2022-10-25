f8ff4ccf20de42393d2c0cf060617bb9582f5b36
Nicholas Robinson <kylenr23@gmail.com>
2022-10-25 01:07:55 +0100

    Added Dropdown functionality for media playing - Creates multiple dropdown menus for sources - Updates list on startup - List can be updated by running the updatemedia command

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,22 @@ module.exports = {
         if (!voice.inVoiceChannel)
             return interaction.reply({ content: `Bot is not in a Voice Channel`, ephemeral: true });
 
-		const source = interaction.options.getString('source');
+		// Get all the possible vlaues from all the Options
+		// Check the amount of parameters and then check for correct value
+		let sourceOption = [];
+		let source = null;
+		for (let i = 0; i < voice.playOptionsAmount; i++) {
+			sourceOption[i] = interaction.options.getString(`source${i}`);
+			if (sourceOption[i] != null) {
+				if (source != null)
+					return interaction.reply({ content: `Select only 1 media file`, ephemeral: true });
+				source = sourceOption[i];
+			}
+		}
+
+		if (source == null)
+			return interaction.reply({ content: `Select a media file`, ephemeral: true });
+
         voice.playMedia(source);
         
         return interaction.reply({ content: `Playing ${voice.completeMediaSource(source)}`, ephemeral: true });
